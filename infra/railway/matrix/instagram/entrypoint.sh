77e4e4438519ab3709033ed4eb6ca9118ed862ac
Luc Succes <l2succes@gmail.com>
2026-08-13 14:19:47 -0600

    fix: align Instagram bridge Matrix domain

--- infra/railway/matrix/instagram/entrypoint.sh
+++ infra/railway/matrix/instagram/entrypoint.sh
@@ -7,6 +7,7 @@ replace() { file="$1"; key="$2"; value="$3"; sed -i "s|$key|$value|g" "$file"; }
 replace /data/config.yaml 'claire.local' "$MATRIX_SERVER_NAME"
 replace /data/config.yaml 'synapse' 'synapse.railway.internal'
 replace /data/config.yaml 'mautrix-instagram' 'mautrixinstagram.railway.internal'
+replace /data/config.yaml '__MATRIX_SERVER_NAME__' "$MATRIX_SERVER_NAME"
 replace /data/config.yaml '__AS_TOKEN__' "$IG_AS_TOKEN"
 replace /data/config.yaml '__HS_TOKEN__' "$IG_HS_TOKEN"
 replace /data/config.yaml '__DOUBLE_PUPPET_AS__' "$DOUBLE_PUPPET_AS_TOKEN"
