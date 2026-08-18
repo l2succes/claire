c91931504a9397002c3beeb4ea0270a963ce22f9
Luc Succes <l2succes@gmail.com>
2026-08-17 22:29:45 -0600

    chore: declare mobile encryption export compliance

--- apps/client/app.config.js
+++ apps/client/app.config.js
@@ -30,6 +30,10 @@ module.exports = ({ config }) => {
     ios: {
       ...config.ios,
       bundleIdentifier: identifier(),
+      infoPlist: {
+        ...config.ios?.infoPlist,
+        ITSAppUsesNonExemptEncryption: false,
+      },
     },
     android: {
       ...config.android,
